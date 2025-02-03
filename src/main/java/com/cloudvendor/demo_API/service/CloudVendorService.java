d86ec47bb636ffb8d4dcea8b318b10607892e37f
DESKTOP-SP1RLBN\Johnny Medina <johnny21noberto@gmail.com>
2025-02-03 22:42:22 +0000

    Develop: adding javadoc to the methods from the controller and service layers.

--- src/main/java/com/cloudvendor/demo_API/service/CloudVendorService.java
+++ src/main/java/com/cloudvendor/demo_API/service/CloudVendorService.java
@@ -8,11 +8,58 @@ import java.util.Map;
 
 public interface CloudVendorService {
 
+    /**
+     * Abstract method to create a new cloud vendor
+     * @param cloudVendor
+     * @return a String with success
+     */
     public String createCloudVendor(CloudVendor cloudVendor);
+
+    /**
+     * Abstract method to return all the cloud vendors
+     * @return a List<CloudVendor>
+     */
     public List<CloudVendor> getAllCloudVendors();
+
+    /**
+     * Abstract method to return a cloud vendor
+     * based on vendorId
+     * @param cloudVendorId
+     * @return a CloudVendor obj
+     */
     public CloudVendor getCloudVendorInfo(String cloudVendorId);
+
+    /**
+     * Abstract method to update the cloud vendor
+     * based on vendorId
+     * @param cloudVendor
+     * @param vendorId
+     * @return a String with success or fail
+     */
     public String updateCloudVendorInfo(CloudVendor cloudVendor, String vendorId);
+
+    /**
+     * Abstract method to delete the cloud vendor
+     * based on vendorId
+     * @param cloudVendorId
+     * @return a String with success or fail
+     */
     public String deleteCloudVendor(Integer cloudVendorId);
+
+    /**
+     * Abstract method to get cloud vendor
+     * based on vendorName
+     * @param cloudVendorName
+     * @return a List<CloudVendor>
+     */
     public List<CloudVendor> getCloudVendorByName(String cloudVendorName);
+
+    /**
+     * Abstract method to update a specific info on the cloud vendor
+     * based on vendorId
+     * @param vendorId
+     * @param updates
+     * @return a String with success or fail
+     */
     String updateSpecificCloudVendorInfo(String vendorId, Map<String, Object> updates);
 }
--- src/main/java/com/cloudvendor/demo_API/service/CloudVendorServiceImpl.java
+++ src/main/java/com/cloudvendor/demo_API/service/CloudVendorServiceImpl.java
@@ -19,6 +19,12 @@ public class CloudVendorServiceImpl implements CloudVendorService{
         this.cloudVendorRepository = cloudVendorRepository;
     }
 
+    /**
+     * Method implementation to create a new cloud vendor obj
+     * and persist on the database
+     * @param cloudVendor
+     * @return a String with success of operation
+     */
     @Override
     public String createCloudVendor(CloudVendor cloudVendor) {
 
@@ -26,11 +32,23 @@ public class CloudVendorServiceImpl implements CloudVendorService{
         return "Cloud Vendor created";
     }
 
+    /**
+     * Method implementation to return all the cloud vendors
+     * existing on the database, if none exists return empty
+     * @return a List<CloudVendor>
+     */
     @Override
     public List<CloudVendor> getAllCloudVendors() {
         return cloudVendorRepository.findAll();
     }
 
+    /**
+     * Method implementation to retrieve a cloud vendor from the
+     * database based on vendorId. If vendorId doesn't exist on database
+     * a CloudVendorNotFoundException is launched.
+     * @param cloudVendorId
+     * @return a CloudVendor obj
+     */
     @Override
     public CloudVendor getCloudVendorInfo(String cloudVendorId) {
         if(cloudVendorRepository.findById(Integer.parseInt(cloudVendorId)).isEmpty())
@@ -38,6 +56,13 @@ public class CloudVendorServiceImpl implements CloudVendorService{
         return cloudVendorRepository.findByVendorId(Integer.parseInt(cloudVendorId));
     }
 
+    /**
+     * Method implementation to update the cloud vendor information on the database
+     * based on vendorId. If the vendorId doesn't exist returns a message
+     * @param cloudVendor
+     * @param vendorId
+     * @return a String with success or fail
+     */
     @Override
     public String updateCloudVendorInfo(CloudVendor cloudVendor, String vendorId) {
 
@@ -51,6 +76,12 @@ public class CloudVendorServiceImpl implements CloudVendorService{
 
     }
 
+    /**
+     * Method implementation to delete the cloud vendor from the
+     * database, based on vendorId. If the vendorId doesn't exist returns a message
+     * @param cloudVendorId
+     * @return a String with success or fail
+     */
     @Override
     public String deleteCloudVendor(Integer cloudVendorId) {
 
@@ -64,12 +95,26 @@ public class CloudVendorServiceImpl implements CloudVendorService{
 
     }
 
+    /**
+     * Method implementation to return all the cloud vendors from the
+     * database with the same vendorName, based on vendorId.
+     * @param cloudVendorName
+     * @return a List<CloudVendor>
+     */
     @Override
     public List<CloudVendor> getCloudVendorByName(String cloudVendorName) {
 
         return cloudVendorRepository.findByVendorName(cloudVendorName);
     }
 
+    /**
+     * Method implementation to update one or more specific fields of the cloud vendor
+     * in the database, based on vendorId. If the vendorId doesn't exist on the database
+     * launches a EntityNotFoundException
+     * @param vendorId
+     * @param updates
+     * @return a String with success or fail
+     */
     @Override
     public String updateSpecificCloudVendorInfo(String vendorId, Map<String, Object> updates) {
         //check if the entity exists
